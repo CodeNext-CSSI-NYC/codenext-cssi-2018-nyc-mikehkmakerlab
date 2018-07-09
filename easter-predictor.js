4b7611640ab9619188920327d8c7db2f0088d1ec
mikehkmakerlab <michaelokrah43@gmail.com>
2018-07-09 16:46:13 -0400

    Easter
    Creating a easter predictor algorithms.

--- easter-predictor.js
+++ easter-predictor.js
@@ -1,1 +1,20 @@
-// Author: FirstName LastName
+// Author: Michael Okrah
+let y = 2018;
+
+a =(y/19);
+b = (y/100);
+c = (y%100);
+d = (b/4);
+e = (b%4);
+f = (b +8)/25 ;
+g = (b - f + 1)/3;
+h = (19 * a + b - d - g + 15)/30;
+i = (c/ 4);
+k = (c%4);
+r = (32 + 2 * e + 2 * i - h - k)%7;
+m =  ( a + 11 * h + 22 * r)/ 451 ;
+n = (h + r - 7 * m + 114)/31  ;
+p = (h + r - 7 * m + 114)%31 ;
+
+console.log(n);
+console.log(p+1);
